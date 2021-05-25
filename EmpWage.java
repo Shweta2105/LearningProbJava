93c2a97574d5a1ecee35a3e53173437303ce550e
Shweta2105 <sghadi21@gmail.com>
2021-05-25 08:07:31 +0530

    Part time added

--- EmpWage.java
+++ EmpWage.java
@@ -1,16 +1,22 @@
 public class EmpWage {
 	public static void main(String[] args) {
 	   int IS_FULLTIME=1;
+		int IS_PARTTIME=2;
 		int empWagePerHr=20;
 		int empHr;
-      double empCheck=Math.floor(Math.random()*10)%2;
+      double empCheck=Math.floor(Math.random()*10)%3;
 		System.out.println(empCheck);
       if (empCheck==IS_FULLTIME)
       {
          System.out.println("Employee is Present");
 			empHr=8;
       }
-      else
+      else if(empCheck==IS_PARTTIME)
+		{
+			System.out.println("Parttime");
+			empHr=4;
+		}
+		else
       {
          System.out.println("Employee is Absent");
 			empHr=0;
