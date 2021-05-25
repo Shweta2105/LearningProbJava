58f70614516b907fb5da7be4a56ade82373b105d
Shweta2105 <sghadi21@gmail.com>
2021-05-25 11:41:17 +0530

    Used switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,30 +1,30 @@
 public class EmpWage {
+	public static final int IS_FULLTIME=1;
+   public static final int IS_PARTTIME=2;
+
 	public static void main(String[] args) {
-	   int IS_FULLTIME=1;
-		int IS_PARTTIME=2;
+
 		int empWagePerHr=20;
 		int empHr;
-      double empCheck=Math.floor(Math.random()*10)%3;
+      double empCheck= Math.floor(Math.random()*10)%3;
 		System.out.println(empCheck);
-      if (empCheck==IS_FULLTIME)
-      {
-         System.out.println("Employee is Present");
-			empHr=8;
-      }
-      else if(empCheck==IS_PARTTIME)
-		{
-			System.out.println("Parttime");
-			empHr=4;
-		}
-		else
-      {
-         System.out.println("Employee is Absent");
-			empHr=0;
-      }
-		int wage=empWagePerHr*empHr;
-		System.out.println(wage);
 
-   }
+		 switch ((int)empCheck)
+      {
+         case IS_FULLTIME:
+            System.out.println("Employee is Present");
+            empHr=8;
+            break;
+         case IS_PARTTIME:
+
+            System.out.println("Parttime");
+            empHr=4;
+            break;
+         default:
+            System.out.println("Employee is Absent");
+            empHr=0;
+      }
+      int wage=empWagePerHr*empHr;
+      System.out.println(wage);
+	}
 }
-
-
