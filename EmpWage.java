8a0cf7fa6e3b72811c3bd8cb44f43fc25a1b6172
Shweta2105 <sghadi21@gmail.com>
2021-05-25 07:40:04 +0530

    Daily wage added

--- EmpWage.java
+++ EmpWage.java
@@ -1,16 +1,23 @@
 public class EmpWage {
 	public static void main(String[] args) {
 	   int IS_FULLTIME=1;
+		int empWagePerHr=20;
+		int empHr;
       double empCheck=Math.floor(Math.random()*10)%2;
 		System.out.println(empCheck);
       if (empCheck==IS_FULLTIME)
       {
          System.out.println("Employee is Present");
+			empHr=8;
       }
       else
       {
          System.out.println("Employee is Absent");
+			empHr=0;
       }
+		int wage=empWagePerHr*empHr;
+		System.out.println(wage);
+
    }
 }
 
